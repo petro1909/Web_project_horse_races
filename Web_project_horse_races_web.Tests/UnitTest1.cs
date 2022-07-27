bcb453e94f4f825aaecb999ef33c698b45c67850
petro1909 <petya-97_97@mail.ru>
2022-07-27 22:42:58 +0300

    Add unit testing

---
+++ Web_project_horse_races_web.Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using System;
+using Xunit;
+using Web_project_horse_races_web.Services.Bets;
+using Web_project_horse_races_db.Model;
+namespace Web_project_horse_races_web.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            //UserBetType btype = new UserBetType() { Name = "Winner" };
+            //UserBet ubet = new UserBet() { UserBetType = btype };
+            //IBetService service = IBetService.CreateService(ubet);
+            //Type type = new WinnerBetService().GetType();
+            //Assert.IsType(type, service);
+        }
+    }
+}
